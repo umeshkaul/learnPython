ace483930145da6114df110077e1496a88abe6b0
Umesh Kaul <umeshkaul@gmail.com>
2017-01-10 22:12:35 -0500

    commit

---
+++ oop1.py
@@ -0,0 +1,21 @@
+class Employee:
+	def __init__ (self,first, last, pay):
+		self.first = first
+		self.last = last
+		self.pay = pay
+		self.email = first + '.' + last + '@company.com'
+	def fullname(self):
+		return '{} {}'.format(self.first, self.last)
+
+
+
+
+
+emp_1 = Employee('umesh','kaul',5000)
+
+
+#print emp_1.first,emp_1.last
+#print ('{} {}'.format(emp_1.first, emp_1.last))
+#print(emp_1.fullname())
+print(Employee.fullname(emp_1))
+print emp_1.fullname()
