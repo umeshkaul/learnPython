2b712f9124fb0506d2220801e6a9da0c1aef0bee
umesh kaul <umeshkaul@gmail.com>
2018-01-13 19:26:02 -0500

    modified oop.py

--- oop1.py
+++ oop1.py
@@ -13,10 +13,12 @@ class Employee:
 
 
 emp_1 = Employee('umesh','kaul',5000)
+emp_2 = Employee('Dmesh','Paul',6000)
 
 
 #print emp_1.first,emp_1.last
 #print ('{} {}'.format(emp_1.first, emp_1.last))
 #print(emp_1.fullname())
 print(Employee.fullname(emp_1))
-print emp_1.fullname()
+print(Employee.fullname(emp_2))
+#print emp_1.fullname()
