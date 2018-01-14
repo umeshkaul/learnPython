414c0c0f3c7fd50ed10e7b29c7ba180c218d0e63
umesh kaul <umeshkaul@gmail.com>
2018-01-14 11:30:44 -0500

    oop3

---
+++ oop3.py
@@ -0,0 +1,23 @@
+class Employee:
+	def __init__ (self,first, last, pay):
+		self.first = first
+		self.last = last
+		self.pay = pay
+		self.email = first + '.' + last + '@company.com'
+	def fullname(self):
+		return '{} {}'.format(self.first, self.last)
+
+	def apply_raise(self):
+		self.pay = int(self.pay * 1.04)
+
+
+
+emp_1 = Employee('three','kaul',5000)
+emp_2 = Employee('three','Paul',6000)
+
+
+#print emp_1.first,emp_1.last
+#print ('{} {}'.format(emp_1.first, emp_1.last))
+#print(emp_1.fullname())
+print(Employee.fullname(emp_1))
+print(Employee.fullname(emp_2))
